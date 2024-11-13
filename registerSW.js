a7b1768b2de87e3fe610ce4b00b267d7dc93a848
Karthik Kallapiran.T <116157045+CodeKnights7@users.noreply.github.com>
2024-11-13 11:26:31 +0530

    Add files via upload

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://kind-meadow-0bb930e1e.5.azurestaticapps.net/sw.js', { scope: 'https://kind-meadow-0bb930e1e.5.azurestaticapps.net/' })})}
